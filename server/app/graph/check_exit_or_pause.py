595e132c558117fed976135678a378570cf6b86b
HeejungYu <0000yuhj@gmail.com>
2025-11-10 18:39:37 +0900

    refactor: CheckExitOrPause 엣지 삭제, 이에 맞춰 build.py에서 graph 빌드 수정

--- server/app/graph/check_exit_or_pause.py
+++
@@ -1,8 +0,0 @@
-# app/graph/check_exit_or_pause.py
-from app.state_types import State
-
-def check_exit_or_pause(state: State) -> State:
-    return state
-
-def cond_exit_or_loop(state: State) -> str: 
-    return "TAIL" if (state.exit or state.expired) else "LOOP"
