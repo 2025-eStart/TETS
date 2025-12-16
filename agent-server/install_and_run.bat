66558406285cc01017a87121d6f669c59778261d
HeejungYu <0000yuhj@gmail.com>
2025-12-16 15:29:12 +0900

    snapshot

--- agent-server/install_and_run.bat
+++ agent-server/install_and_run.bat
@@ -5,14 +5,34 @@ echo ===================================================
 echo   [Agent Server] Auto Installer & Launcher
 echo ===================================================
 
-:: 1. .env 파일 존재 여부 확인
+:: 1. .env 파일 존재 여부, 위치 확인 및 이동
+if exist .env (
+    echo [System] Copying .env file to server core...
+    copy /Y .env server_core\.env >nul
+)
+cd source_code
+
 if not exist .env (
     echo.
     echo [ERROR] .env file not found!
-    echo ---------------------------------------------------
-    echo Please create a '.env' file in this folder using
-    echo the keys provided via email.
-    echo ---------------------------------------------------
+    echo ---------------------------------------------------------
+    echo ❗️ .env 파일이 존재하지 않습니다.❗️
+    echo 메일로 보내드린 키를 이용해 .env.template을 수정하고
+    echo .env로 해당 파일명을 수정해주세요.
+    echo ---------------------------------------------------------
+    echo.
+    pause
+    exit
+)
+if not exist .firebase_key.json (
+    echo.
+    echo [ERROR] .firebase_key.json file not found!
+    echo ----------------------------------------------------------
+    echo ❗️ .firebase_key.json 파일이 존재하지 않습니다.❗️
+    echo 메일로 보내드린 .firebase_key.template.json을
+    echo 실행 파일(install_and_run.bat)과 동일한 위치에 복사한 후 저장해주세요.
+    echo ❗️ 파일명은 반드시 .firebase_key.json 이어야 합니다.
+    echo ----------------------------------------------------------
     echo.
     pause
     exit
